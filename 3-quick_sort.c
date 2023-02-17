a2289ae08077c1b788e6c6bccd0faa86fec2b3c3
Erickkaranja <karanja99erick@gmail.com>
2023-02-17 09:37:25 +0300

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,89 @@
+#include "sort.h"
+
+/**
+* swap_element - function that swaps two elements.
+* @element_1: The first element.
+* @element_2: The second element.
+*
+*/
+
+void swap_element(int *element_1, int *element_2)
+{
+	int temp;
+
+	temp = *element_1;
+	*element_1 = *element_2;
+	*element_2 = temp;
+}
+
+/**
+ * lomuto_partition - Order a subset of an array of integers according to
+ *                    the lomuto partition scheme (last element as pivot).
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order.
+ *
+ * Return: The final partition index.
+ */
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, below;
+
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap_element(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+
+	if (array[above] > *pivot)
+	{
+		swap_element(array + above, pivot);
+		print_array(array, size);
+	}
+
+	return (above);
+}
+
+/**
+ * lomuto_sort - Implement the quicksort algorithm through recursion.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ *
+ * Description: Uses the Lomuto partition scheme.
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+/**
+* quick_sort - function that implements the quick sort algorithm.
+*
+* @array: The array to be sorted.
+*
+* @size: The size of the array to be sorted.
+*/
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	lomuto_sort(array, size, 0, size - 1);
+}
