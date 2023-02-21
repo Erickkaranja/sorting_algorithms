27ae78678db4da345d442c8a2e59d086da4b8502
Erickkaranja <karanja99erick@gmail.com>
2023-02-21 13:15:52 +0300

    changes on header

--- sort.h
+++ sort.h
@@ -19,7 +19,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-void swap_element(int *element_1, int *element_2);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
